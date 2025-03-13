aab6ba3cb8f36d41d2bb2d01f3be546bc3743675
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-03-14 00:13:50 +0100

    add SessionProvider to RootLayout and clean up layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ import {
   User,
   SlidersHorizontal,
 } from "lucide-react-native";
+import { SessionProvider } from "@/components/provider/session.provider";
 
 let defaultTheme: "dark" | "light" = "light";
 
@@ -72,48 +73,34 @@ export default function RootLayout() {
 
   return (
     <>
-      {/* top SafeAreaView */}
-      {/* <SafeAreaView
-        className={`${colorMode === "light" ? "bg-[#E5E5E5]" : "bg-[#262626]"}`}
-      />
-      <ThemeContext.Provider value={{ colorMode, toggleColorMode }}>
-        <GluestackUIProvider mode={colorMode}>
-          <SafeAreaView
-            className={`${
-              colorMode === "light" ? "bg-white" : "bg-[#171717]"
-            } flex-1 overflow-hidden`}
-          >
-            <Box className="flex-1">
+      <SessionProvider>
+        {/* top SafeAreaView */}
+        <SafeAreaView
+          className={`${
+            colorMode === "light" ? "bg-[#E5E5E5]" : "bg-[#262626]"
+          }`}
+        />
+        <ThemeContext.Provider value={{ colorMode, toggleColorMode }}>
+          <GluestackUIProvider mode={colorMode}>
+            <SafeAreaView
+              className={`${
+                colorMode === "light" ? "bg-white" : "bg-[#171717]"
+              } flex-1 overflow-hidden`}
+            >
               <Stack screenOptions={{ headerShown: false }}>
                 <Stack.Screen
                   name="(tabs)"
                   options={{
-                    headerShown: false
+                    headerShown: false,
                   }}
-                  />
-                <Stack.Screen
-                name="not-found" />
-              </Stack>
-              <Box className="h-[72px] items-center w-full flex md:hidden border-t border-outline-50">
-                <MobileBottomTabs
-                  activeTab={activeTab}
-                  setActiveTab={setActiveTab}
-                  bottomTabs={bottomTabs}
                 />
-              </Box>
-            </Box>
-          </SafeAreaView>
-        </GluestackUIProvider>
-      </ThemeContext.Provider> */}
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="not-found" />
-      </Stack>
+                <Stack.Screen name="loginPage" />
+                <Stack.Screen name="not-found" />
+              </Stack>
+            </SafeAreaView>
+          </GluestackUIProvider>
+        </ThemeContext.Provider>
+      </SessionProvider>
     </>
   );
 }
