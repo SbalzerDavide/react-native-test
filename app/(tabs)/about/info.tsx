86ef5565319a9fc46048127b71075e605db4142d
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-03-14 00:12:37 +0100

    hide header on InfoScreen

--- app/(tabs)/about/info.tsx
+++ app/(tabs)/about/info.tsx
@@ -6,7 +6,10 @@ import { Text } from "@/components/ui/text";
 const InfoScreen = () => {
   return (
     <>
-    <Stack.Screen options={{ headerTitle: "Oops!" }}/>
+    <Stack.Screen options={{ 
+      headerTitle: "Oops!",
+      headerShown: false
+       }}/>
     <View className="flex-1 justify-center items-center">
       <Text>Info</Text>
     </View>
