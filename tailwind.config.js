db755e680fcc5a7a45e7b484de03d50a3e06fc4f
SbalzerDavide <sbalzerdavide@hotmail.it>
2025-03-14 00:11:32 +0100

    add contrast color to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
           800: 'rgb(var(--color-primary-800)/<alpha-value>)',
           900: 'rgb(var(--color-primary-900)/<alpha-value>)',
           950: 'rgb(var(--color-primary-950)/<alpha-value>)',
+          contrast: 'rgb(var(--color-primary-contrast)/<alpha-value>)',
         },
         secondary: {
           0: 'rgb(var(--color-secondary-0)/<alpha-value>)',
